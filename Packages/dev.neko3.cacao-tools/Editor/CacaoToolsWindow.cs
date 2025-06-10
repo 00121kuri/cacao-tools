b6747b07ae8710f6ba5f3083fb28fc4e9685ce03
00121kuri <00121kuri@gmail.com>
2025-06-10 18:46:20 +0900

    feat: cacao-toolsパッケージの作成

---
+++ Packages/dev.neko3.cacao-tools/Editor/CacaoToolsWindow.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace Dev.Neko3.CacaoTools.Editor
+{
+    /// <summary>
+    /// Cacao Tools用のエディターウィンドウ
+    /// </summary>
+    public class CacaoToolsWindow : EditorWindow
+    {
+        [MenuItem("Tools/Cacao Tools/Cacao Tools Window")]
+        public static void ShowWindow()
+        {
+            GetWindow<CacaoToolsWindow>("Cacao Tools");
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Cacao Tools", EditorStyles.boldLabel);
+            GUILayout.Space(10);
+
+            if (GUILayout.Button("Test Log"))
+            {
+                Debug.Log("Cacao Tools is working!");
+            }
+
+            GUILayout.Space(10);
+            GUILayout.Label("このパッケージは正常に動作しています。", EditorStyles.helpBox);
+        }
+    }
+}
---
+++ Packages/dev.neko3.cacao-tools/README.md
@@ -0,0 +1,38 @@
+# Cacao Tools
+
+VRChat用のユーティリティツール集です。
+
+## 機能
+
+- アバター制作を効率化するツール群
+- VRChatアバター向けの便利な機能
+
+## インストール方法
+
+### VPM (VRChat Package Manager) を使用
+
+1. VRChat Creator Companionを開く
+2. "Manage Project" でプロジェクトを選択
+3. このパッケージを追加
+
+### 手動インストール
+
+1. このリポジトリをクローンまたはダウンロード
+2. `Packages`フォルダに配置
+
+## 使用方法
+
+Unity Editorのメニューから各種ツールにアクセスできます。
+
+## 要件
+
+- Unity 2022.3以降
+- VRChat SDK3 (Avatars)
+
+## ライセンス
+
+MIT License
+
+## 作者
+
+neko3
